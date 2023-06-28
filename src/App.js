383a5482705d1bbe7b42ca081da09f802d4982a5
GauravGupta <gaurav_gupta1286@yahoo.com>
2023-06-28 13:54:36 +0530

    small changes of update and remove

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import './employee.css'
 
 
 function App() {
@@ -17,11 +18,11 @@ function App() {
     e.preventDefault();
     try {
         
-        const response = await axios.post('https://bbtn3h503k.execute-api.us-east-1.amazonaws.com/dev', {
+         await axios.post('https://bbtn3h503k.execute-api.us-east-1.amazonaws.com/dev', {
           empId:empId,
           emp_Name: empName,
           designation:designation,
-          salary: salary,
+          salary: salary
         });
 
     addEmployee({ empId, empName, designation, salary });
@@ -30,16 +31,11 @@ function App() {
     setDesignation('');
     setSalary('');
 
-    try {
-      const response = await axios.get('https://bbtn3h503k.execute-api.us-east-1.amazonaws.com/dev');
-      setEmployeeData(response.data);
-    } catch (error) {
-      console.error('Error fetching employees:', error);
-    }
-
   }catch (error) {
     console.error(error);
   }
+  fetchEmployees();
+
  } ;
 
   const addEmployee = (employee) => {     //addEmp
@@ -55,73 +51,129 @@ function App() {
     }
   };
 
+  const removeData = async (Id) => {
+
+    console.log('epid which is going to be deleted=' + Id);
+    try {
+    await axios.delete('https://bbtn3h503k.execute-api.us-east-1.amazonaws.com/dev/', 
+    {
+      data: {
+        empId: Id
+      }
+    });
+    
+  } catch (error) {
+    console.error('Error in deleting employees:', error);
+  }
+      fetchEmployees();
+
+}
+
+const updateData = (empid) => {
+  //  let url = `https://jsonplaceholder.typicode.com/users/${id}`
+  console.log('id', empid)
+  //  axios.delete(url).then(res => {
+  //      const del = employees.filter(employee => id !== employee.id)
+  //      setEmployees(del)
+  //      console.log('res', res)
+  //  })
+}
+
   return (
     <div className="App">
-      <div >
-          <h1>Employee Management App</h1>
-          
-          <form onSubmit={handleSubmit}>
-      <label>
-        Employee ID:
-        <input
-          type="text"
-          value={empId}
-          onChange={(e) => setEmpId(e.target.value)}
-        />
-      </label>
-      <label>
-        Employee Name:
-        <input
-          type="text"
-          value={empName}
-          onChange={(e) => setEmpName(e.target.value)}
-        />
-      </label>
-      <label>
-        Designation:
-        <input
-          type="text"
-          value={designation}
-          onChange={(e) => setDesignation(e.target.value)}
-        />
-      </label>
-      <label>
-        Salary:
-        <input
-          type="text"
-          value={salary}
-          onChange={(e) => setSalary(e.target.value)}
-        />
-      </label>
-      <button type="submit">Add Employee</button>
-    </form>
+          <div className="topHeader">
+              <h1>Employee Management App</h1>
+          </div>
+          <div className="grid-container">
+            <div className="grid-child purple">
+                    <form className="employee-form" onSubmit={handleSubmit}>
+                      
+                      <div className="form-group">
+                        <label htmlFor="name">Employee Id:</label>
+                          
+                            <input
+                              type="text"
+                              value={empId}
+                              onChange={(e) => setEmpId(e.target.value)}
+                            />
+                      </div>
+                      
+                      <div className="form-group">
+                      <label htmlFor="name">Employee Name:</label>
+                        <input
+                          type="text"
+                          value={empName}
+                          onChange={(e) => setEmpName(e.target.value)}
+                        />
+                      </div>
+                      <div className="form-group">
+                      <label htmlFor="name">Designation:</label>
+                        
+                        <input
+                          type="text"
+                          value={designation}
+                          onChange={(e) => setDesignation(e.target.value)}
+                        />
+                      </div>
+                      <div className="form-group">
+                      <label htmlFor="name">Salary:</label>
+                        <input
+                          type="text"
+                          value={salary}
+                          onChange={(e) => setSalary(e.target.value)}
+                        />
+                      </div>
+                        <div className="ButtonPositionDiv" >
+                          <button className="addEmpButton" type="submit">Add Employee</button>
+                        </div>
+                </form>
 
-          <p>&nbsp;</p>
-      </div>
-      <div className="container">
-            <table>
-              <thead>
-                <tr>
-                  <th>Employee ID</th>
-                  <th>Name</th>
-                  <th>Designation</th>
-                  <th>Salary</th>
-                </tr>
-              </thead>
-              <tbody>
-                {employeeData.map((employee) => (
-                  <tr key={employee.empid}>
-                    <td>{employee.empId}</td>
-                    <td>{employee.emp_Name}</td>
-                    <td>{employee.designation}</td>
-                    <td>{employee.salary}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <p>&nbsp;</p>
-            <button className="buttonPos" onClick={fetchEmployees}>FETCH Emp</button>
-        </div>
+                <p>&nbsp;</p>
+                </div>
+                <div className="grid-child green">
+                  <div className="employee-list">
+                        <table className='tableClass'>
+                          <thead>
+                            <tr>
+                              <th> EID </th>
+                              <th>Name</th>
+                              <th>Designation</th>
+                              <th>Salary</th>
+                              <th>Remove</th>
+                              <th>Update</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {employeeData.map((employee) => (
+                              <tr key={employee.empid}>
+                                <td>{employee.empId}</td>
+                                <td>{employee.emp_Name}</td>
+                                <td>{employee.designation}</td>
+                                <td>{employee.salary}</td>
+                                <td className="operation">
+                                    <button className="deleteButton" onClick={() => removeData(employee.empId)}>
+                                      Delete
+                                    </button>
+                                </td>
+                                <td className="operation">
+                                    <button className="updateButton" onClick={() => updateData(employee.empId)}>
+                                      Update
+                                    </button>
+                                </td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                        <p>&nbsp;</p>
+                       
+                    </div>
+                        <div className="ButtonPositionDiv">
+                            <button className="fetchEmpButton" onClick={fetchEmployees}>Fetch Employees</button>
+                        </div>
+                    </div>
+            
+            
+            </div>
     </div>
   );
 }
