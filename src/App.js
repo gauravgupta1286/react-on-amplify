54863ca899bcc496a66941aad5cd9270274683b6
GauravGupta <gaurav_gupta1286@yahoo.com>
2023-07-15 17:34:58 +0530

    updated all crud

--- src/App.js
+++ src/App.js
@@ -13,9 +13,26 @@ function App() {
   const [empName, setEmpName] = useState('');
   const [designation, setDesignation] = useState('');
   const [salary, setSalary] = useState('');
+  const [isAddEmpDisabled, setAddEmpDisabled] = useState(false);
+  const [isIdEmpDisabled, setisIdEmpDisabled] = useState(false);
+  const [highlightedRow, setHighlightedRow] = useState(null);
+  
+  
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
+   
+    if(empId.trim().length===0 || 
+      empName.trim().length===0 ||
+      designation.trim().length===0 ||
+      salary.trim().length===0)
+      {
+
+        alert('Please fill complete details !!!!');
+
+      }else{
+      
+      
     try {
         
          await axios.post('https://bbtn3h503k.execute-api.us-east-1.amazonaws.com/dev', {
@@ -24,7 +41,7 @@ function App() {
           designation:designation,
           salary: salary
         });
-
+    alert('Employee with EmployeeId = ' + empId + ' and Employee Name = '+ empName +' has been added Successfully.');
     addEmployee({ empId, empName, designation, salary });
     setEmpId('');
     setEmpName('');
@@ -32,9 +49,11 @@ function App() {
     setSalary('');
 
   }catch (error) {
+    alert('There was some error in adding employee and error message is =' + error);
     console.error(error);
   }
   fetchEmployees();
+}
 
  } ;
 
@@ -61,25 +80,68 @@ function App() {
         empId: Id
       }
     });
+
+    alert('Employee with Empid = '+ Id + ' has been deleted Successfully.');
     
   } catch (error) {
+    alert('Error in deleting employees:' + error);
     console.error('Error in deleting employees:', error);
   }
       fetchEmployees();
 
 }
 
-const updateData = (empid) => {
-  //  let url = `https://jsonplaceholder.typicode.com/users/${id}`
-  console.log('id', empid)
-  //  axios.delete(url).then(res => {
-  //      const del = employees.filter(employee => id !== employee.id)
-  //      setEmployees(del)
-  //      console.log('res', res)
-  //  })
+
+const selectData = ( obj, index, event) => {
+  console.log('empId:', obj.empId);
+  console.log('IValue:', index, event.currentTarget);
+  //event.currentTarget.disabled = true;
+  setHighlightedRow(index);
+  setEmpId(obj.empId);
+  setEmpName(obj.emp_Name);
+  setDesignation(obj.designation);
+  setSalary(obj.salary);
+  setAddEmpDisabled(true);
+  setisIdEmpDisabled(true);
+  
+    
+}
+const updateData = async(objEmp, event) => {
+  console.log('emp:', objEmp.empId);
+
+ 
+  try {
+      
+       await axios.put('https://bbtn3h503k.execute-api.us-east-1.amazonaws.com/dev', {
+        empId:empId,
+        emp_Name: empName,
+        designation:designation,
+        salary: salary
+      });
+
+ 
+  addEmployee({ empId, empName, designation, salary });
+  setEmpId('');
+  setEmpName('');
+  setDesignation('');
+  setSalary('');
+  
+
+}catch (error) {
+  alert('Employee with Empid = '+ objEmp.empId + ' has not been updated. Error = '+ error);
+  console.error(error);
+}
+alert('Employee with Empid = '+ objEmp.empId + ' has been updated Successfully.');
+  fetchEmployees();
+ setAddEmpDisabled(false);
+ setisIdEmpDisabled(false);
+ setHighlightedRow(10000);
+  
+  
 }
 
   return (
+    
     <div className="App">
           <div className="topHeader">
               <h1>Employee Management App</h1>
@@ -94,6 +156,7 @@ const updateData = (empid) => {
                             <input
                               type="text"
                               value={empId}
+                              disabled={isIdEmpDisabled}
                               onChange={(e) => setEmpId(e.target.value)}
                             />
                       </div>
@@ -124,7 +187,8 @@ const updateData = (empid) => {
                         />
                       </div>
                         <div className="ButtonPositionDiv" >
-                          <button className="addEmpButton" type="submit">Add Employee</button>
+                          <button className="addEmpButton" disabled={isAddEmpDisabled}  type="submit">Add Employee</button>
+                         
                         </div>
                 </form>
 
@@ -139,25 +203,32 @@ const updateData = (empid) => {
                               <th>Name</th>
                               <th>Designation</th>
                               <th>Salary</th>
-                              <th>Remove</th>
+                              <th>Select</th>
                               <th>Update</th>
+                              <th>Remove</th>
                             </tr>
                           </thead>
                           <tbody>
-                            {employeeData.map((employee) => (
-                              <tr key={employee.empid}>
+                            {employeeData.map((employee, index) => (
+                              <tr key={employee.empid} className={highlightedRow === index ? 'highlighted' : 'unhighlighted'}> 
                                 <td>{employee.empId}</td>
                                 <td>{employee.emp_Name}</td>
                                 <td>{employee.designation}</td>
                                 <td>{employee.salary}</td>
+                                
                                 <td className="operation">
-                                    <button className="deleteButton" onClick={() => removeData(employee.empId)}>
-                                      Delete
+                                    <button className="selectButton" onClick={(event) => selectData(employee, index, event)}>
+                                      Select
                                     </button>
                                 </td>
                                 <td className="operation">
-                                    <button className="updateButton" onClick={() => updateData(employee.empId)}>
+                                    <button className="updateButton" onClick={(event) => updateData(employee)}>
                                       Update
+                                    </button>
+                                </td>
+                                <td className="operation">
+                                    <button className="deleteButton" onClick={() => removeData(employee.empId)}>
+                                      Delete
                                     </button>
                                 </td>
                               </tr>
